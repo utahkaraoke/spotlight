3a6b23d2fce258e7915aa21fb1c3dbb306a0b906
Richard <legalize@xmission.com>
2020-05-24 15:28:55 -0600

    Handle <artist> <title> as well as <artist> - <title> in title strings

--- burst-csv.py
+++ burst-csv.py
@@ -4,16 +4,20 @@ import re
 def squishSpaces(text):
   return re.sub(' +', ' ', text)
 
+def removePrefix(text, prefix):
+  if text.startswith(prefix):
+    text = re.sub('^' + re.escape(prefix), '', text).strip()
+  return text
+
 def readCsv(file):
   artists = {}
-
   with open(file, 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in reader:
       title = squishSpaces(row[0]).strip()
       artist = squishSpaces(row[1]).strip()
-      if title.startswith(artist + ' - '):
-        title = title.replace(artist + ' - ', '').strip()
+      title = removePrefix(title, artist + ' - ')
+      title = removePrefix(title, artist + ' ')
       if len(artist) > 0 and len(title) > 0:
         if not artist in artists:
           artists[artist] = []
